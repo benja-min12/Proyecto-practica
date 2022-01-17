4155ae5c71df8828d16e98c8488f0ded50b438f9
benjamín millas venegas <benjamin.millas@alumnos.ucn.cl>
2022-01-17 17:31:11 -0300

    creacion de modelo ajustes guardado de datos

--- src/hacker-news/hacker-news.controller.ts
+++ src/hacker-news/hacker-news.controller.ts
@@ -11,41 +11,82 @@ export class HackerNewsController {
   async getApiData() {
     const { data } = await this.hackerNewsService.findall().toPromise();
     const { hits } = data;
-    //recorrer todos los hits
-    for (const item of hits) {
-      if (item.story_title !== null && item.story_url !== null) {
-        const createdHackernewsDto: CreatedHackernewsDto = {
-          title: item.story_title,
-          url: item.story_url,
-          author: item.author,
-          created_at: item.created_at,
-        };
-        const hackerNews1 = await this.hackerNewsService.findOne(
-          item.story_title,
-        );
-        if (hackerNews1 === null) {
-          const hackerNews = await this.hackerNewsService.create(
-            createdHackernewsDto,
-          );
-        }
-      }
-      if (item.title !== null && item.url !== null) {
-        const createdHackernewsDto: CreatedHackernewsDto = {
-          title: item.title,
-          url: item.url,
-          author: item.author,
-          created_at: item.created_at,
-        };
-        const hackerNews1 = await this.hackerNewsService.findOne(item.title);
-        if (hackerNews1 === null) {
-          const hackerNews = await this.hackerNewsService.create(
-            createdHackernewsDto,
-          );
-        }
+    //mostrar solo los hits que tienen story_url y story_title
+    const hitsFiltrados = hits.filter(
+      (item) => item.story_url && item.story_title,
+    );
+    //recorrer los hits filtrados
+    for (const item of hitsFiltrados) {
+      const createdHackernewsDto: CreatedHackernewsDto = {
+        title: item.story_title,
+        url: item.story_url,
+        author: item.author,
+        created_at: item.created_at,
+      };
+      //verificar si existe el registro
+      const existe = await this.hackerNewsService.findOne(item.story_title);
+      if (!existe) {
+        //crear el registro
+        await this.hackerNewsService.create(createdHackernewsDto);
       }
     }
-    const hackerNews = await this.hackerNewsService.gethackernews();
-    return hackerNews;
+    //mostrar solo los hits que tienen title y url
+    const hitsFiltrados2 = hits.filter((item) => item.title && item.url);
+    //recorrer los hits filtrados
+    for (const item of hitsFiltrados2) {
+      const createdHackernewsDto: CreatedHackernewsDto = {
+        title: item.title,
+        url: item.url,
+        author: item.author,
+        created_at: item.created_at,
+      };
+      //verificar si existe el registro
+      const existe = await this.hackerNewsService.findOne(item.title);
+      if (!existe) {
+        //crear el registro
+        await this.hackerNewsService.create(createdHackernewsDto);
+      }
+    }
+    //mostrar solo los hits que tienen title y story_url
+    const hitsFiltrados3 = hits.filter((item) => item.title && item.story_url);
+    //recorrer los hits filtrados
+    for (const item of hitsFiltrados3) {
+      const createdHackernewsDto: CreatedHackernewsDto = {
+        title: item.title,
+        url: item.story_url,
+        author: item.author,
+        created_at: item.created_at,
+      };
+      //verificar si existe el registro
+      const existe = await this.hackerNewsService.findOne(item.title);
+      if (!existe) {
+        //crear el registro
+        const hackerNews = await this.hackerNewsService.create(
+          createdHackernewsDto,
+        );
+      }
+    }
+    //mostrar solo los hits que tienen url y story_title
+    const hitsFiltrados4 = hits.filter((item) => item.url && item.story_title);
+    //recorrer los hits filtrados
+    for (const item of hitsFiltrados4) {
+      const createdHackernewsDto: CreatedHackernewsDto = {
+        title: item.story_title,
+        url: item.url,
+        author: item.author,
+        created_at: item.created_at,
+      };
+      //verificar si existe el registro
+      const existe = await this.hackerNewsService.findOne(item.title);
+      if (!existe) {
+        //crear el registro
+        const hackerNews = await this.hackerNewsService.create(
+          createdHackernewsDto,
+        );
+      }
+    }
+    const hackerNewsver = await this.hackerNewsService.gethackernews();
+    return hackerNewsver;
   }
   @Get('/hacker-newsver')
   async getApiDataVer() {
